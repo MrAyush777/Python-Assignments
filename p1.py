e4d69ea22e039fbd21545c09be2dba09d4d78f6c
MrAyush <ayushfataniya2005@gmail.com>
2026-02-06 11:43:58 +0530

    adding assignment 1

---
+++ p1.py
@@ -0,0 +1,16 @@
+# Task 1: Perform Basic Mathematical Operations
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+
+# addition
+print("Addition: ",num1+num2)
+
+# subtraction
+print("Subtraction: ",num1-num2)
+
+# multiplication
+print("Multiplication: ",num1*num2)
+
+# division  
+print("Division: ",num1/num2)
---
+++ p2.py
@@ -0,0 +1,8 @@
+# Task 2: Create a Personalized Greeting
+
+fname = input("Enter your first name : ")
+lname = input("Enter your last name : ")
+
+fullname = fname+ " " + lname
+
+print("Hello, " + fullname + "! Welcome to the python program.")
