c810848a53f7e67cca26f4b692ca6e283c6984bc
MrAyush <ayushfataniya2005@gmail.com>
2026-05-02 17:40:45 +0530

    p6 has added

---
+++ p6.py
@@ -0,0 +1,9 @@
+import math
+
+def calculations(n):
+    print(f"square root : {math.sqrt(n)}")
+    print(f"logarithm : {math.log(n)}")
+    print(f"sine : {math.sin(n)}")
+    
+num = int(input("enter a number : "))
+result = calculations(num)
