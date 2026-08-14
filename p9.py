0230f8a45ce2fee71a47d9f95ca6464ac06cf121
MrAyush777 <ayushfataniya2005@gmail.com>
2026-08-14 15:23:01 +0530

    Task 2 completed

---
+++ p9.py
@@ -0,0 +1,12 @@
+# Task 2: Demonstrate List Slicing 
+
+nums = [1,2,3,4,5,6,7,8,9,10]
+print(f"Original list : {nums}")
+
+extractedNums = nums[0:5]
+print(f"Extracted first five elements : {extractedNums}")
+
+extractedNums.reverse()
+print(f"Reversed extracted elements : {extractedNums}")
+
+
