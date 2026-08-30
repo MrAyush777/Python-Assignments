78c04af34052b13ef1182c276f771e1fedaa4e11
MrAyush777 <ayushfataniya2005@gmail.com>
2026-08-30 18:24:42 +0530

    Assignment 4 - task 1 has completed

--- p7.py
+++ p7.py
@@ -1,8 +1,13 @@
-lineNo=1
+# Task 1: Read a File and Handle Errors
+line_no = 0
 try:
     
-    with open("sample.txt","r") as fs:
-        for line in fs:
-            print(f"{line.strip()}")
+    with open("sample.txt","rt") as sp:
+        content = sp.readlines()
+    
+        for line in content:
+            line_no+=1
+            print(f"line {line_no}: {line}",end="")
+
 except FileNotFoundError:
-    print()
+    print(f"Error : the file 'sample.txt' was not found.")
